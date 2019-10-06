11aa344f60df415d754dc382b7c71b63491509c1
andwari <andwariaciel@gmail.com>
2019-10-06 11:33:19 +0200

    Database backup and restore without password

---
+++ src/main/java/com/andwari/settings/control/DatabaseBackupHandler.java
@@ -0,0 +1,81 @@
+package com.andwari.settings.control;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.andwari.core.tournamentcore.database.DatabaseManager;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class DatabaseBackupHandler {
+
+	public void restoreDatabase(Stage stage) {
+		// TODO: Master Password
+		FileChooser fileChooser = new FileChooser();
+		File selectedFile = fileChooser.showOpenDialog(stage);
+		if (selectedFile != null) {
+			if (DatabaseManager.restore(selectedFile)) {
+				showDialogSucces("Database successfully restored");
+			} else {
+				showDialogFailed("Error restoring Database! Please conatct Admin");
+			}
+		}
+	}
+
+	public void backupDatabase(String path) {
+		File backupDir = new File(path);
+		boolean backupSuccesful = false;
+		if (backupDir.exists()) {
+			File backup = new File(backupDir, getBackupFilename());
+			try {
+				backup.createNewFile();
+				backupSuccesful = DatabaseManager.backup(backup);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (backupSuccesful) {
+				showDialogSucces("Backup successful!\n" + backup.getAbsolutePath());
+			} else {
+				showDialogFailed("Backup failed! Contact Admin");
+			}
+		} else {
+			showDialogFailed("Please enter a path to an existing folder.");
+		}
+	}
+
+	private void showDialogFailed(String message) {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Failed");
+		alert.setHeaderText(null);
+		alert.setContentText(message);
+		alert.show();
+		return;
+	}
+
+	private void showDialogSucces(String message) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Success");
+		alert.setHeaderText(null);
+		alert.setContentText(message);
+		alert.show();
+		return;
+
+	}
+
+	private String getBackupFilename() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("backup-");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_hh-mm");
+		Date date = new Date();
+		sb.append(dateFormat.format(date));
+		sb.append(".zip");
+		return sb.toString();
+	}
+
+}
